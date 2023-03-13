970bffbb4f84bb1fe8eecdc64395815bd2d71e7e
D4riu5 <R3iji94@gmail.com>
2023-03-13 18:34:57 +0100

    V 0.01 - added: project files, created trains table, updated a column from trains table (train_full_name)

---
+++ database/migrations/2023_03_13_183234_add_train_full_name_to_trains_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            DB::statement('UPDATE trains SET train_full_name = CONCAT(company, " ", train_code, " ", arrival_station)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            DB::statement('UPDATE trains SET train_full_name = NULL');
+        });
+    }
+};
